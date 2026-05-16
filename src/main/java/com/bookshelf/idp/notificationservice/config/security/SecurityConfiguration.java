16ba7076a19a3f363bd3068325dfea861446ee71
adacosma <adacosma03@gmail.com>
2026-05-16 15:13:26 +0300

    changes

--- src/main/java/com/bookshelf/idp/notificationservice/config/security/SecurityConfiguration.java
+++ src/main/java/com/bookshelf/idp/notificationservice/config/security/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(java.util.List.of("http://localhost:5173", "http://localhost:5174"));
                     config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(java.util.List.of("*"));
                     config.setAllowCredentials(true);
